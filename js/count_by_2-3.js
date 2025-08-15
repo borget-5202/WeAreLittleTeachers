6827f98b30fe005f991c45bca621745b9354e394
borget-5202 <borgetteach@gmail.com>
2025-08-14 21:45:46 -0400

    Update count_by_2-3.js
    add apple png

--- js/count_by_2-3.js
+++ js/count_by_2-3.js
@@ -2,6 +2,7 @@ document.getElementById("bucket").addEventListener("drop", (e) => {
   e.preventDefault();
   const appleId = e.dataTransfer.getData("text/plain");
   const apple = document.getElementById(appleId);
+  apple.src = "/assets/apple.png";
   if (apple && e.target.children.length < groupSize) {
     e.target.appendChild(apple);
     if (e.target.children.length === groupSize) {
